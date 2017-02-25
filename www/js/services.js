07a6687988e3792f81ae4361fee349ee5d26e1df
lakshmig <lakshmig@users.noreply.github.com>
2017-02-25 15:34:17 +0530

    Added the services.js file

---
+++ www/js/services.js
@@ -0,0 +1,11 @@
+angular.module('starter.services', [])
+.factory('wifiFactory', function ($http, $q) {
+// intialize local variables.
+// Add private functions.
+   return {
+          // Functions written here will be accessable in the controller.
+          scanAndConnectToWifi: function(){
+          //do all the wifireleated functionality
+          }
+      }
+});
